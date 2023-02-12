23b363e74595ab31b2ec1f3e1b699352349c53f6
Ronit Shrestha <113393639+lemonzest786@users.noreply.github.com>
2023-02-12 00:14:38 -0500

    added files

---
+++ assignment01/enter-chore.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Chore</title>
+</head>
+
+<body>
+    <main>
+
+        <h1>Add new completed chore</h1>
+        <form action="save-chore.php" method="post">
+            <fieldset>
+                <label for="choremen">Choremen:</label>
+                <select name="choremen" id="choremen">
+                    <?php
+                    // connecting the database
+                    $db = new PDO('mysql:host=172.31.22.43;dbname=Ronit200535182', 'Ronit200535182', 'nvqBTSUXEw');
+
+                    // using SELECT to fetch the data
+                    $sql = "SELECT * FROM choremens";
+
+                    // running the query
+                    $cmd = $db->prepare($sql);
+                    $cmd->execute();
+                    $choremens = $cmd->fetchAll();
+
+                    // loop through the data to create a list item
+                    foreach ($choremens as $choremen) {
+                        echo '<option>' . $choremen['choremen'] . '</option>';
+                    }
+
+
+                    ?>
+                </select>
+            </fieldset>
+
+            <fieldset>
+                <label for="typechore">Type of chore:</label>
+                <select name="typechore" id="typechore">
+                    <?php
+
+                    // using SELECT to fetch the data
+                    $sql = "SELECT * FROM typechores";
+
+                    // running the query
+                    $cmd = $db->prepare($sql);
+                    $cmd->execute();
+                    $typechores = $cmd->fetchAll();
+
+                    // loop through the data to create a list item
+                    foreach ($typechores as $typechore) {
+                        echo '<option>' . $typechore['typechore'] . '</option>';
+                    }
+
+
+                    ?>
+                </select>
+            </fieldset>
+
+            <fieldset>
+                <label for="day">Day:</label>
+                <select name="day" id="day">
+                    <?php
+
+
+                    // using SELECT to fetch the data
+                    $sql = "SELECT * FROM days";
+
+                    // running the query
+                    $cmd = $db->prepare($sql);
+                    $cmd->execute();
+                    $days = $cmd->fetchAll();
+
+                    // loop through the data to create a list item
+                    foreach ($days as $day) {
+                        echo '<option>' . $day['day'] . '</option>';
+                    }
+
+                    // disconnecting the database
+                    $db = null;
+                    ?>
+                </select>
+            </fieldset>
+
+            <fieldset>
+                <label for="note">Note:</label>
+                <textarea name="note" id="note" required maxlength="4000"></textarea>
+            </fieldset>
+            <button class="btnSave">Save</button>
+        </form>
+    </main>
+</body>
+
+</html>
---
+++ assignment01/save-chore.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saving the chore data</title>
+</head>
+<body>
+    <?php
+    // capture the form body input using the $_POST array & store in a var
+    $choremen = $_POST['choremen'];
+    $typechore = $_POST['typechore'];
+    $day = $_POST['day'];
+    $note = $_POST['note'];
+
+    // calculate the date and time with php
+    date_default_timezone_set("America/Toronto");
+    $dateCreated = date("y-m-d h:i");
+
+    // lesson 4 - add validation before saving. Check 1 at a time for descriptive errors.
+    $ok = true;  // start with no validation errors
+
+    if (empty($choremen)) {
+        echo '<p class="error">Post choremen is required.</p>';
+        $ok = false; // error happened - bad data
+    }
+
+    if (empty($typechore)) {
+        echo '<p class="error">typechore is required.</p>';
+        $ok = false; // error happened - bad data
+    }
+    if (empty($day)) {
+        echo '<p class="error">day is required.</p>';
+        $ok = false; // error happened - bad data
+    }
+    if (empty($note)) {
+        echo '<p class="error">note is required.</p>';
+        $ok = false; // error happened - bad data
+    }
+
+    // only save to db if $ok has never been changed to false
+    if ($ok == true) {
+        // connect to the db using the PDO library
+        $db = new PDO('mysql:host=172.31.22.43;dbname=Ronit200535182', 'Ronit200535182', 'nvqBTSUXEw');
+      
+       // set up an SQL INSERT
+        $sql = "INSERT INTO chores (choremen, typechore, day, note, dateCreated) VALUES (:choremen, :typechore, :day, :note, :dateCreated)";
+
+        // map each input to the corresponding db column
+        $cmd = $db->prepare($sql);
+        $cmd->bindParam(':choremen', $choremen, PDO::PARAM_STR, 100);
+        $cmd->bindParam(':typechore', $typechore, PDO::PARAM_STR, 100);
+        $cmd->bindParam(':day', $day, PDO::PARAM_STR, 100);
+        $cmd->bindParam(':note', $note, PDO::PARAM_STR, 4000);
+        $cmd->bindParam(':dateCreated', $dateCreated, PDO::PARAM_STR);
+
+        // execute the insert
+        $cmd->execute();
+
+        // disconnect
+        $db = null;
+
+        // show the user a message
+        echo 'Data saved';
+    }
+    ?>
+</body>
+</html>
